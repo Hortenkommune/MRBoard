eb191f5dd91dc92fabeb658c8254bdfc0468d8d3
Forsbakk <jonas.forsbakk@horten.kommune.no>
2019-06-23 14:39:28 +0200

    2nd commit

---
+++ auth.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+var request = require('request');
+
+const TOKEN_ENDPOINT = process.env.TOKEN_ENDPOINT;
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+
+// The auth module object.
+var auth = {};
+
+auth.getAccessToken = function () {
+  return new Promise(function (resolve, reject){
+    var requestParams = {
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      scope: 'https://graph.microsoft.com/.default',
+      grant_type: 'client_credentials'
+    };
+    request.post({ url: TOKEN_ENDPOINT, form: requestParams }, function (err, response, body) {
+      var parsedBody = JSON.parse(body);
+      if (err) {
+        reject(err);
+      } else if (parsedBody.error) { 
+        reject(parsedBody.error);
+      } else {
+        resolve(parsedBody.access_token);
+      }
+    });
+  });
+}
+module.exports = auth;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
+var auth = require('../auth');
+var graph = require('../graph.js');
+
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', 
+  async function(req, res) {
+    let params = {};
+    try {
+      var authToken = await auth.getAccessToken();
+    } catch (err) {
+      console.log('failed to aquire access token');
+    }
+    try {
+      var events = await graph.getEvents(authToken);
+      params.events = events.value;
+    } catch (err) {
+      console.log(err)
+    }
+    res.render('index', params );
+  });
 
 module.exports = router;
